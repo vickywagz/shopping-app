f8f284f158dc013791776d707d4cf531d14752f8
victory wagor <wagorvictory@gmail.com>
2025-11-21 21:42:31 +0100

    working on add product

--- lib/Admin/add_product.dart
+++ lib/Admin/add_product.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:random_string/random_string.dart';
+import 'package:shopping_app/services/database.dart';
 import 'package:shopping_app/widget/support_widget.dart';
 
 class AddProduct extends StatefulWidget {
@@ -9,6 +15,48 @@ class AddProduct extends StatefulWidget {
 }
 
 class _AddProductState extends State<AddProduct> {
+  final ImagePicker _picker = ImagePicker();
+  File? selectedImage;
+  TextEditingController namecontroller = new TextEditingController();
+
+  Future getImage() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+    selectedImage = File(image!.path);
+    setState(() {});
+  }
+
+  uploadItem() async {
+    if (selectedImage != null && namecontroller.text != "") {
+      String addId = randomAlphaNumeric(10);
+      Reference firebaseStorageRef = FirebaseStorage.instance
+          .ref()
+          .child("blogImage")
+          .child("addId");
+
+      final UploadTask task = firebaseStorageRef.putFile(selectedImage!);
+      var downloadUrl = await (await task).ref.getDownloadURL();
+
+      Map<String, dynamic> addProduct = {
+        "Name": namecontroller.text,
+        "Image": downloadUrl,
+      };
+
+      await DatabaseMethods().addProduct(addProduct, value!).then((value) {
+        selectedImage = null;
+        namecontroller.text = "";
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.redAccent,
+            content: Text(
+              "Product has been uploaded successfully!!!",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+        );
+      });
+    }
+  }
+
   String? value;
 
   final List<String> categoryitem = ['Watch', 'Laptop', 'TV', 'Headphones'];
@@ -57,6 +105,7 @@ class _AddProductState extends State<AddProduct> {
                 borderRadius: BorderRadius.circular(20),
               ),
               child: TextField(
+                controller: namecontroller,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
@@ -100,7 +149,9 @@ class _AddProductState extends State<AddProduct> {
             SizedBox(height: 30.0),
             Center(
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  uploadItem();
+                },
                 child: Text("Add Product", style: TextStyle(fontSize: 22.0)),
               ),
             ),
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -7,4 +7,13 @@ class DatabaseMethods {
         .doc(id)
         .set(userInfoMap);
   }
+
+  Future addProduct(
+    Map<String, dynamic> userInfoMap,
+    String categoryname,
+  ) async {
+    return await FirebaseFirestore.instance
+        .collection(categoryname)
+        .add(userInfoMap);
+  }
 }
